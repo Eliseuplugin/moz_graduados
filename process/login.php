815e34c9f96c147566a86cae68c579cf6a61873f
Eliseu <eliseuplugin@gmail.com>
2018-02-02 16:46:07 -0700

    codigo da tela de login

---
+++ process/login.php
@@ -0,0 +1,19 @@
+<?php 
+session_start();
+include '../library/configServer.php';
+include '../library/consulSQL.php';
+include '../library/SED.php';
+$UserType=consultasSQL::CleanStringText($_POST['UserType']);
+$loginName=consultasSQL::CleanStringText($_POST['loginName']);
+$loginPassword=consultasSQL::CleanStringText($_POST['loginPassword']);
+$fecha=date("d-m-Y");
+$hora=date("H:i:s");
+$pass=SED::encryption($loginPassword);
+if($UserType=="Estudante"){
+	$key="BI";
+	$table="estudante";
+	$userN="graduado";
+}
+
+
+ ?>
